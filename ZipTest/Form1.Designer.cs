aa80ce59c10d696fd6c9404fb080c88a71502633
kenichi_shintome <kshintome@say-tech.co.jp>
2015-02-27 14:33:26 +0900

    画面なんとなく作成

--- ZipTest/Form1.Designer.cs
+++ ZipTest/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textSrc = new System.Windows.Forms.TextBox();
+            this.btnSrcRef = new System.Windows.Forms.Button();
+            this.btnDistRef = new System.Windows.Forms.Button();
+            this.textDist = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "フォルダー指定";
+            // 
+            // textSrc
+            // 
+            this.textSrc.Location = new System.Drawing.Point(15, 20);
+            this.textSrc.Name = "textSrc";
+            this.textSrc.Size = new System.Drawing.Size(416, 19);
+            this.textSrc.TabIndex = 1;
+            // 
+            // btnSrcRef
+            // 
+            this.btnSrcRef.Location = new System.Drawing.Point(437, 18);
+            this.btnSrcRef.Name = "btnSrcRef";
+            this.btnSrcRef.Size = new System.Drawing.Size(75, 23);
+            this.btnSrcRef.TabIndex = 2;
+            this.btnSrcRef.Text = "参照";
+            this.btnSrcRef.UseVisualStyleBackColor = true;
+            this.btnSrcRef.Click += new System.EventHandler(this.btnSrcRef_Click);
+            // 
+            // btnDistRef
+            // 
+            this.btnDistRef.Location = new System.Drawing.Point(437, 97);
+            this.btnDistRef.Name = "btnDistRef";
+            this.btnDistRef.Size = new System.Drawing.Size(75, 23);
+            this.btnDistRef.TabIndex = 5;
+            this.btnDistRef.Text = "参照";
+            this.btnDistRef.UseVisualStyleBackColor = true;
+            this.btnDistRef.Click += new System.EventHandler(this.btnDistRef_Click);
+            // 
+            // textDist
+            // 
+            this.textDist.Location = new System.Drawing.Point(15, 99);
+            this.textDist.Name = "textDist";
+            this.textDist.Size = new System.Drawing.Size(416, 19);
+            this.textDist.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "出力先";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 202);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(497, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "進捗";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 335);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDistRef);
+            this.Controls.Add(this.textDist);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSrcRef);
+            this.Controls.Add(this.textSrc);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textSrc;
+        private System.Windows.Forms.Button btnSrcRef;
+        private System.Windows.Forms.Button btnDistRef;
+        private System.Windows.Forms.TextBox textDist;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ZipTest/Form1.cs
+++ ZipTest/Form1.cs
@@ -15,5 +15,48 @@ namespace ZipTest
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 圧縮元ディレクトリ指定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSrcRef_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+
+            fbd.Description = "圧縮したいフォルダーを指定してください。";
+
+            fbd.ShowNewFolderButton = false;
+
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                textSrc.Text = fbd.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// 圧縮先指定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDistRef_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.FileName = "";
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            sfd.Filter = "Zipファイル(*.zip;)|*zip";
+            sfd.Title = "保存先のファイルを指定してください。";
+            sfd.OverwritePrompt = true;
+            sfd.CheckPathExists = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
     }
 }
